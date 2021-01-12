4dc76a0f2c9997ccffbdfa4076101bf8f57d4428
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2021-01-12 10:07:31 +0545

    Optimized Code

--- src/Account/AccountView.java
+++ src/Account/AccountView.java
@@ -7,10 +7,11 @@ public class AccountView {
     // Instiantiating Account Handler to invoke its methords
     public AccountHandeler storage = new AccountHandeler();
 
+    // Defining Input Scanner
+    Scanner input = new Scanner(System.in);
+
     // Adds Account
     public void addAccountView() {
-        // Defining Input Scanner
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter Account Number:");
         int accountNumber = input.nextInt();
         System.out.println("Enter Account Name:");
@@ -18,47 +19,29 @@ public class AccountView {
         System.out.println("Enter Opeaning Balance:");
         float accountBalance = input.nextFloat();
         // Check if Account Added
-        if (storage.addAccount(accountNumber, accountName, accountBalance)) {
-            System.out.println("#######################################################");
-            System.out.println("Account Added Successfully");
-            System.out.println("#######################################################\n");
-        } else {
-            System.out.println("#######################################################");
-            System.out.println("Account Already Exists");
-            System.out.println("#######################################################\n");
-        }
+        if (storage.addAccount(accountNumber, accountName, accountBalance))
+            printInLine("Account Added Successfully");
+        else
+            printInLine("Account Already Exists");
     }
 
     // Checks Amount
     public void checkAmountView() {
-        // Defining Input Scanner
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter Account Number:");
         int accountNumber = input.nextInt();
         AcccountClass account = storage.findAccount(accountNumber);
         float amount = storage.checkAmount(accountNumber);
         if (account != null) {
-            if (amount != 0) {
-                System.out.println("#######################################################");
-                System.out.println(
-                        "Account Number " + account.getAccountNumber() + " has balance of Rs: " + amount + "/-");
-                System.out.println("#######################################################\n");
-            } else {
-                System.out.println("#######################################################");
-                System.out.println("Account Number " + account.getAccountNumber() + " has no baance left.");
-                System.out.println("#######################################################\n");
-            }
-        } else {
-            System.out.println("#######################################################");
-            System.out.println("Enter Valid Account Number.");
-            System.out.println("#######################################################\n");
-        }
+            if (amount != 0)
+                printInLine("Account Number " + account.getAccountNumber() + " has balance of Rs: " + amount + "/-");
+            else
+                printInLine("Account Number " + account.getAccountNumber() + " has no baance left.");
+        } else
+            printInLine("Enter Valid Account Number.");
     }
 
     // Deposits Amount
     public void depositAmountView() {
-        // Defining Input Scanner
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter Account Number:");
         int accountNumber = input.nextInt();
         System.out.println("Enter Deposite Balance:");
@@ -66,29 +49,18 @@ public class AccountView {
         AcccountClass account = storage.findAccount(accountNumber);
         float depositAmount = storage.depositeAmount(accountNumber, amount);
         if (account != null) {
-            if (depositAmount != 0) {
-                System.out.println("#######################################################");
-                System.out.println("New Deposite Request of Rs: " + amount + "/- on Account number "
-                        + account.getAccountNumber() + ".");
-                System.out.println("Account Number " + account.getAccountNumber() + " has new balance of Rs: "
-                        + depositAmount + "/-");
-                System.out.println("#######################################################\n");
-            } else {
-                System.out.println("#######################################################");
-                System.out.println("Problem With Server Please Try again later.");
-                System.out.println("#######################################################\n");
-            }
-        } else {
-            System.out.println("#######################################################");
-            System.out.println("Enter Valid Account Number.");
-            System.out.println("#######################################################\n");
-        }
+            if (depositAmount != 0)
+                printInLine("New Deposite Request of Rs: " + amount + "/- on Account number "
+                        + account.getAccountNumber() + ".\n" + "Account Number " + account.getAccountNumber()
+                        + " has new balance of Rs: " + depositAmount + "/-");
+            else
+                printInLine("Problem With Server Please Try again later.");
+        } else
+            printInLine("Enter Valid Account Number.");
     }
 
     // Withdraw Amount
     public void withdrawAmountView() {
-        // Defining Input Scanner
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter Account Number:");
         int accountNumber = input.nextInt();
         System.out.println("Enter Withdraw Balance:");
@@ -99,31 +71,22 @@ public class AccountView {
             // Accounts Exists
             if (withdraw > 0) {
                 // Withdraw
-                System.out.println("#######################################################");
-                System.out.println("New Withdraw Request of Rs: " + amount + "/-on Account number "
-                        + account.getAccountNumber() + ".");
-                System.out.println(
-                        "Account Number " + account.getAccountNumber() + " has new balance of Rs: " + withdraw + "/-");
-                System.out.println("#######################################################\n");
+                printInLine("New Withdraw Request of Rs: " + amount + "/-on Account number "
+                        + account.getAccountNumber() + ".\n" + "Account Number " + account.getAccountNumber()
+                        + " has new balance of Rs: " + withdraw + "/-");
             } else {
                 // Balance insufficent
-                System.out.println("#######################################################");
-                System.out.println("Account Number " + account.getAccountNumber() + " has insufficent balance(Rs "
+                printInLine("Account Number " + account.getAccountNumber() + " has insufficent balance(Rs "
                         + account.getAccountBalance() + "/-) to withdraw(Rs " + amount + "/-).");
-                System.out.println("#######################################################\n");
             }
         } else {
             // Accound Dont Exists
-            System.out.println("#######################################################");
-            System.out.println("Enter Valid Account Number.");
-            System.out.println("#######################################################\n");
+            printInLine("Enter Valid Account Number.");
         }
     }
 
     // Transfure Amount
     public void transfurAmountView() {
-        // Defining Input Scanner
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter Sender Account Number:");
         int senderAccount = input.nextInt();
         System.out.println("Enter Receiver Account Number:");
@@ -134,27 +97,18 @@ public class AccountView {
         AcccountClass account_B = storage.findAccount(receiverAccount);
         int transfur = storage.transfureAmountA_to_B(senderAccount, receiverAccount, amount);
         if (transfur == 1) {
-            System.out.println("#######################################################");
-            System.out.println("New Withdraw Request of Rs: " + amount + "/- on Account number "
-                    + account_A.getAccountNumber() + ".");
-            System.out.println("New Deposite Request of Rs: " + amount + "/- on Account number "
-                    + account_B.getAccountNumber() + ".");
-            System.out.println("Sender Account Number " + account_A.getAccountNumber() + " has new balance of Rs: "
-                    + account_A.getAccountBalance() + "/-");
-            System.out.println("Receiver Account Number " + account_B.getAccountNumber() + " has new balance of Rs: "
-                    + account_B.getAccountBalance() + "/-");
-            System.out.println("#######################################################\n");
+            printInLine("New Withdraw Request of Rs: " + amount + "/- on Account number " + account_A.getAccountNumber()
+                    + ".\n" + "New Deposite Request of Rs: " + amount + "/- on Account number "
+                    + account_B.getAccountNumber() + ".\n" + "Sender Account Number " + account_A.getAccountNumber()
+                    + " has new balance of Rs: " + account_A.getAccountBalance() + "/-\n" + "Receiver Account Number "
+                    + account_B.getAccountNumber() + " has new balance of Rs: " + account_B.getAccountBalance() + "/-");
         } else if (transfur == 0) {
             // Balance insufficent
-            System.out.println("#######################################################");
-            System.out.println("Sender Account Number " + account_A.getAccountNumber() + " has insufficent balance(Rs "
+            printInLine("Sender Account Number " + account_A.getAccountNumber() + " has insufficent balance(Rs "
                     + account_A.getAccountBalance() + "/-) to withdraw(Rs " + amount + "/-).");
-            System.out.println("#######################################################\n");
         } else {
             // Accound Dont Exists
-            System.out.println("#######################################################");
-            System.out.println("Enter Valid Account Number.");
-            System.out.println("#######################################################\n");
+            printInLine("Enter Valid Account Number.");
         }
     }
 
@@ -164,35 +118,34 @@ public class AccountView {
         int size = accountList.size();
         if (size != 0) {
             // Accounts Exists
-            System.out.println("\n#######################################################");
-            System.out.println(size + " Accounts Exists and are listed below.");
-            System.out.println("#######################################################");
+            printInLine(size + " " + (size == 1 ? "account" : "accounts") + " found and listed below.");
             int count = 1;
             for (AcccountClass account : accountList) {
-                System.out.println(count++ + ") Account Number: " + account.getAccountNumber() + " Account Name: "
-                        + account.getAccountName() + " Account Balance: " + account.getAccountBalance() + "/-");
+                System.out.println(count++ + "-> Account Number: " + account.getAccountNumber() + " |  Account Name: "
+                        + account.getAccountName() + " | Account Balance: Rs " + account.getAccountBalance() + "/-");
             }
             System.out.println("#######################################################\n");
         } else {
             // Accounts Dont Exist
-            System.out.println("\n#######################################################");
-            System.out.println("Accounts not found in storage.");
-            System.out.println("#######################################################\n");
+            printInLine("Accounts not found in storage.");
         }
     }
 
+    // Deletes Selected Account
     public void deleteAccount() {
-        // Defining Input Scanner
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter Account Number:");
         int accountNumber = input.nextInt();
-        if (storage.deleteAccount(accountNumber)) {
+        if (storage.deleteAccount(accountNumber))
+            printInLine("Accounts Deleted Successfully.");
+        else
+            printInLine("Accounts not found in storage.");
+    }
+
+    // Prints Message Within Lines
+    public void printInLine(String message) {
+        if (message.length() != 0) {
             System.out.println("\n#######################################################");
-            System.out.println("Accounts Deleted Successfully.");
-            System.out.println("#######################################################\n");
-        } else {
-            System.out.println("\n#######################################################");
-            System.out.println("Accounts not found in storage.");
+            System.out.println(message);
             System.out.println("#######################################################\n");
         }
     }
